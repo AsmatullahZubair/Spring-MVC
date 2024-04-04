204178f8ad06bbb0f2dd434e9cf0ceae84dd7e74
Sooraj <soorajbinary@gmail.com>
2024-04-04 12:13:14 +0500

    Spring Data JPA Demo

---
+++ 05-spring-data-jpa/src/main/java/com/solutions/a3z/A3zApplication.java
@@ -0,0 +1,14 @@
+package com.solutions.a3z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3zApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3zApplication.class, args);
+	}
+	
+	
+}
